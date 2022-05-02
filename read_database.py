c9273fea9399c61d73fd34f3fbea4cba2d451870
David Kerschbaumer <david.kerschbaumer@student.tugraz.at>
2022-05-02 10:02:15 +0200

    read database

--- read_database.py
+++ read_database.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 import sqlite3
 
@@ -6,9 +5,21 @@ import sqlite3
 def main():
     con = sqlite3.connect('dataset/corpus.sqlite3')
 
+    articles_df = pd.read_sql_query("SELECT * FROM Articles", con)
+    posts_df = pd.read_sql_query("SELECT * FROM Posts", con)
+    newspaper_staff_df = pd.read_sql_query("SELECT * FROM Newspaper_Staff", con)
+    annotations_df = pd.read_sql_query("SELECT * FROM Annotations", con)
+    annotations_consolidated_df = pd.read_sql_query("SELECT * FROM Annotations_consolidated", con)
+    cross_val_split_df = pd.read_sql_query("SELECT * FROM CrossValSplit", con)
+    categories_df = pd.read_sql_query("SELECT * FROM Categories", con)
 
-    df = pd.read_sql_query("SELECT * FROM Articles", con)
-    print(df)
+
+    print(articles_df)
+    print(posts_df)
+    print(newspaper_staff_df)
+
+
+
 
 if __name__ == "__main__":
     main()
